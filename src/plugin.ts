fd5b00967e30e624949d0982af866e40f057b58e
Dean <hello@dean.press>
2020-02-07 10:45:36 -0800

    chore: remove blockApplied listener

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,14 +1,11 @@
-import { app } from '@arkecosystem/core-container';
-import { Container, EventEmitter, Logger } from '@arkecosystem/core-interfaces';
+import { Container, Logger } from '@arkecosystem/core-interfaces';
 import { Handlers } from '@arkecosystem/core-transactions';
-import { roundCalculator } from '@arkecosystem/core-utils';
 import { createHandyClient } from 'handy-redis';
 import { defaults } from './defaults';
 import { StakeCreateTransactionHandler } from './handlers/stake-create';
 import { StakeRedeemTransactionHandler } from './handlers/stake-redeem';
 import * as StakeHelpers from './helpers';
 
-const emitter = app.resolvePlugin<EventEmitter.EventEmitter>("event-emitter");
 const redis = createHandyClient();
 
 export const plugin: Container.IPluginDescriptor = {
@@ -20,12 +17,6 @@ export const plugin: Container.IPluginDescriptor = {
     Handlers.Registry.registerTransactionHandler(StakeCreateTransactionHandler);
     container.resolvePlugin<Logger.ILogger>("logger").info("Registering Stake Redeem Transaction");
     Handlers.Registry.registerTransactionHandler(StakeRedeemTransactionHandler);
-    emitter.on("block.applied", async block => {
-      const isNewRound = roundCalculator.isNewRound(block.height);
-      if (isNewRound) {
-        await StakeHelpers.ExpireHelper.processExpirations(block);
-      }
-    });
   },
   async deregister(container: Container.IContainer, options) {
     container.resolvePlugin<Logger.ILogger>("logger").info("Deregistering Stake Create Transaction");
